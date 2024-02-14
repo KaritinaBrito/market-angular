c3075a7f75bcdb8df908d6c0688f93c38d6c099f
Karen Brito <karitinabrito@gmail.com>
2024-02-14 11:50:21 -0600

    test data

--- src/app/apiServer.ts
+++ src/app/apiServer.ts
@@ -1,4 +1,4 @@
 export const apiServer = {
-  SERVER_URL: 'browser/browser/assets/data/products.json',
+  SERVER_URL: 'https://main--strong-lollipop-3ac0d6.netlify.app/assets/data/products.json',
   SITE_NAME : 'Little Market'
 }
