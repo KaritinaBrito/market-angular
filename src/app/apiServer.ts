b0f7d7584f1893b326a5f5601a06e1ad82668821
Karen Brito <karitinabrito@gmail.com>
2024-02-14 11:23:20 -0600

    test data

--- src/app/apiServer.ts
+++ src/app/apiServer.ts
@@ -1,4 +1,4 @@
 export const apiServer = {
-  SERVER_URL: 'dist/market/browser/browser/assets/data/products.json',
+  SERVER_URL: 'browser/browser/assets/data/products.json',
   SITE_NAME : 'Little Market'
 }
