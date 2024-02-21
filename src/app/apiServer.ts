018eaa99fef964162b8964047a5a4f8c16362abc
Karen Brito <karitinabrito@gmail.com>
2024-02-20 23:19:28 -0600

    test data.json

--- src/app/apiServer.ts
+++ src/app/apiServer.ts
@@ -1,5 +1,5 @@
 export const apiServer = {
-  SERVER_URL: 'https://main--strong-lollipop-3ac0d6.netlify.app/market/browser/browser/assets/data/products.json',
-  LOCAL_URL: 'https://localhost/assets/data/products.json',
+  SERVER_URL: 'https://strong-lollipop-3ac0d6.netlify.app/home/home/market/browser/browser/assets/data/products.json',
+  LOCAL_URL: 'http://localhost/assets/data/products.json',
   SITE_NAME : 'Little Market'
 }
